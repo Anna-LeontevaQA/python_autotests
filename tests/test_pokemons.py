78a6af2c68f4864ebbf0a05ee4f08be6581a3bdb
Anna-LeontevaQA <166292321+Anna-LeontevaQA@users.noreply.github.com>
2024-05-15 20:08:30 +0300

    Update test_pokemons.py

--- tests/test_pokemons.py
+++ tests/test_pokemons.py
@@ -9,3 +9,7 @@ TRAINER_ID = 2428
 def test_status_code():
     response = requests.get(url = f'{URL}/trainers', params = {'trainer_id': TRAINER_ID})
     assert response.status_code == 200
+
+def test_part_of_response():
+    response_get = requests.get(url = f'{URL}/trainers', params = {'trainer_id': TRAINER_ID})
+    assert response_get.json()["data"][0]["trainer_name"] == 'Лунтик'
